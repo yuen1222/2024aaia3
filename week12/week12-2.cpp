d61ae2740142ecc28dd165b64ce58f158b0ebe7a
yuen1222 <11160242@me.mcu.edu.tw>
2024-11-28 16:29:03 +0800

    week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //ListNode*ans=list1;
+        //ListNode*ans=new ListNode(99);
+        ListNode*ans=new ListNode(99,new ListNode(88));
+        return ans;
+    }
+};
---
+++ week12/week12-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int a[100][100];
+int main()
+{
+	int T,N;
+	cin>>T;
+	for(int t=1;t<=7;t++){
+		char c,c2;
+		cin>>c>>c2>>N;
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N;j++){
+				cin>>a[i][j];
+			}
+		}
+		cout<<"Test #"<<t<<"Symmetric.\n";
+	}
+}
